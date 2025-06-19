1a38fcac1eab2378eb7aa8c542fe585365a3cf55
Eduardo Velasques <eduveks@gmail.com>
2025-06-19 22:46:23 +0100

    Button supports role and dropdown.

--- src/button/Button.js
+++ src/button/Button.js
@@ -5,11 +5,14 @@ import baseProps from "../utils/baseProps";
 function Button(props) {
     const {
         type = 'button',
+        role = 'button',
         mode,
         outline = false,
         size,
         block = false,
         disabled = false,
+        dropdownToggle,
+        ariaExpanded,
         _
     } = props;
     let cssClass = 'btn';
@@ -17,9 +20,9 @@ function Button(props) {
         cssClass += ` btn${outline ? '-outline' : ''}-${mode}`;
     }
     if (size) {
-        if (size == 'lg' || size == 'large') {
+        if (size === 'lg' || size === 'large') {
             cssClass += ' btn-lg';
-        } else if (size == 'sm' || size == 'small') {
+        } else if (size === 'sm' || size === 'small') {
             cssClass += ' btn-sm';
         }
     }
@@ -27,17 +30,33 @@ function Button(props) {
         cssClass += ' btn-block';
     }
     const disabledProps = {};
-    if (disabled == true) {
+    if (disabled === true) {
         cssClass += ' disabled';
         disabledProps.disabled = true;
         disabledProps['aria-disabled'] = "true";
     }
+    const extraProps = {
+        role
+    };
+    if (dropdownToggle) {
+        cssClass += ' dropdown-toggle';
+        extraProps['data-bs-toggle'] = 'dropdown';
+        if (dropdownToggle === 'split') {
+            cssClass += ' dropdown-toggle-split';
+        }
+    }
+    if (ariaExpanded === true) {
+        extraProps['aria-expanded'] = 'true';
+    }
+    if (ariaExpanded === false) {
+        extraProps['aria-expanded'] = 'false';
+    }
     this.view(()=> {
-        if (type == 'link') {
+        if (type === 'link') {
             return [
                 { a: {
+                    ...extraProps,
                     ...baseProps(props, { class: cssClass }),
-                    role: "button",
                     ...disabledProps,
                     _
                 } }
@@ -45,6 +64,7 @@ function Button(props) {
         }
         return [
             { button: {
+                ...extraProps,
                 ...baseProps(props, { class: cssClass }),
                 type,
                 ...disabledProps,
---
+++ test/src/base/Main/style.js
@@ -0,0 +1,12 @@
+import theme from "../../styles/theme";
+import vars from "../../styles/vars";
+
+const style = {
+    main: {
+        ...theme.page,
+        minHeight: 'calc(100vh - 285px)',
+        width: '100%',
+    }
+};
+
+export default style;
