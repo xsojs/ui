2e08943f60b346f947021fb7ce93d7b9fac024df
Eduardo Velasques <eduveks@gmail.com>
2025-06-22 22:52:34 +0100

    Test: CodeBlock component was created.

---
+++ test/src/components/CodeBlock/index.js
@@ -0,0 +1,39 @@
+import com from "@xso/com";
+import css from "@xso/css";
+
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import xml from 'highlight.js/lib/languages/xml';
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('html', xml);
+
+import style from "./style";
+
+function CodeBlock({source, language = 'javascript'}) {
+    let finalSource = '';
+    let padding = 0;
+    for (const line of source.split('\n')) {
+        if (padding == 0 && line != '') {
+            for (let i = 0; i < line.length; i++) {
+                if (line[i] == ' ') {
+                    padding++;
+                } else {
+                    break;
+                }
+            }
+        }
+        finalSource += line.substring(padding) + '\n';
+    }
+    const ref = this.ref();
+    this.changes([ref], () => {
+        hljs.highlightElement(ref.current);
+    });
+    this.view(() => [
+        ref.set({ pre: {
+            class: css(style.codeBlock) + ' language-'+ language,
+            _: finalSource.trim()
+        } })
+    ]);
+}
+
+export default com(CodeBlock);
---
+++ test/src/components/CodeBlock/style.js
@@ -0,0 +1,14 @@
+import theme from "../../styles/theme";
+
+const styles = {
+    codeBlock: {
+        padding: '20px',
+        fontFamily: 'Roboto Mono',
+        overflow: 'auto',
+        '> *': {
+            fontFamily: 'Roboto Mono',
+        }
+    }
+};
+
+export default styles;
