9cefb50913160d601dbe2cce763ce9f88b5dbf84
Eduardo Velasques <eduveks@gmail.com>
2025-06-22 22:58:06 +0100

    Test: App was refactored.

--- test/src/App.js
+++ test/src/App.js
@@ -1,10 +1,42 @@
 import com from "@xso/com";
 
+import Header from "./base/Header";
+import Footer from "./base/Footer";
 import Main from "./base/Main";
+import {Route, Router} from "../../../router/src/main.js";
+import Home from "./pages/Home";
+import Badge from "./pages/Badge";
+import Breadcrumb from "./pages/Breadcrumb";
+import Button from "./pages/Button";
 
 function App() {
     this.view(() => [
-        { [Main]: {} }
+        { [Header]: {} },
+        { [Main]: {
+            _: [
+                { [Router]: {
+                    routes: [
+                        { [Route]: {
+                            path: '/',
+                            component: Home
+                        } }, // Route
+                        { [Route]: {
+                            path: '/badge',
+                            component: Badge
+                        } }, // Route
+                        { [Route]: {
+                            path: '/breadcrumb',
+                            component: Breadcrumb
+                        } }, // Route
+                        { [Route]: {
+                                path: '/button',
+                                component: Button
+                            } }, // Route
+                    ]
+                } }, // Router
+                { [Footer]: {} },
+            ]
+        } }, // Main
     ]);
 }
 
