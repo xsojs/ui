734d8091d680c9bc845f68f3fa66170f2cc59860
Eduardo Velasques <eduveks@gmail.com>
2025-08-17 20:19:23 +0100

    Dropdown Menu with Button and Text, and menu end parameter.

--- src/dropdown/Menu.js
+++ src/dropdown/Menu.js
@@ -6,12 +6,26 @@ import Item from "./Item";
 import Header from "./Header";
 import Divider from "./Divider";
 import Link from "./Link";
+import Button from "./Button";
+import Text from "./Text";
+import {isBoolean, isObject} from "@xso/utils";
 
 const Menu = com(function Menu(props) {
-    const { dark, _ } = props;
+    const { dark, end, _ } = props;
     let cssClass = 'dropdown-menu';
     if (dark) {
         cssClass += ' dropdown-menu-dark';
+    }
+    if (end) {
+        if (isBoolean(end)) {
+            cssClass += ` dropdown-menu-end`;
+        } else if (isObject(end)) {
+            for (const key of Object.keys(end)) {
+                if (end[key] === true) {
+                    cssClass += ` dropdown-menu-${key}-end`;
+                }
+            }
+        }
     }
     this.view(()=> [
         { ul: {
@@ -25,5 +39,7 @@ Menu.Item = Item;
 Menu.Header = Header;
 Menu.Divider = Divider;
 Menu.Link = Link;
+Menu.Button = Button;
+Menu.Text = Text;
 
 export default Menu;
