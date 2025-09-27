27bd2520da5401cb441aa676795c70dfb67b6554
Eduardo Velasques <eduveks@gmail.com>
2025-09-27 23:04:16 +0100

    Input is integrated with form text and label.

--- src/form/Input.js
+++ src/form/Input.js
@@ -1,22 +1,28 @@
 import com from "@xso/com";
 
 import baseProps from "../utils/baseProps";
+import generateID from "../utils/generateID";
+import {isObject, isString} from "@xso/utils";
+import Label from "./Label";
+import Text from "./Text";
 
 const Input = com(function Input(props) {
     const {
-        id = null, placeholder = null,
-        type = 'text',
+        id = 'input_'+ generateID(), placeholder = null,
+        type = 'text', size,
+        label = null, text = null,
         ariaLabel = null, ariaDescribedBy = null,
+        rows = null
     } = props;
+    let labelProps = {};
+    let textProps = {};
     let cssClass = 'form-control';
-    const extraProps = {};
-    if (id) {
-        extraProps.id = id;
-    }
+    const extraProps = { type };
+    extraProps.id = id;
     if (placeholder) {
         extraProps.placeholder = placeholder;
     }
-    if (type == 'checkbox') {
+    if (type === 'checkbox') {
         cssClass = ' form-check-input';
         extraProps.type = 'checkbox';
         if (props.switch) {
@@ -26,38 +32,78 @@ const Input = com(function Input(props) {
             cssClass = 'btn-check';
             extraProps.autocomplete = 'off';
         }
-    } else if (type == 'radio') {
+    } else if (type === 'radio') {
         cssClass = ' form-check-input';
         extraProps.type = 'radio';
         if (props.button) {
             cssClass = 'btn-check';
             extraProps.autocomplete = 'off';
         }
-    } else if (type == 'range') {
+    } else if (type === 'range') {
         cssClass = ' form-range';
+    }
+    if (size) {
+        if (size === 'lg' || size === 'large') {
+            cssClass += ' form-control-lg';
+        } else if (size === 'sm' || size === 'small') {
+            cssClass += ' form-control-sm';
+        }
+    }
+    if (label) {
+        if (isString(label)) {
+            labelProps._ = label;
+        } else if (isObject(label)) {
+            labelProps = {...label};
+        }
+        labelProps.for = id;
     }
     if (ariaLabel) {
         extraProps['aria-label'] = ariaLabel;
     }
+    if (text) {
+        const id = 'form_text_'+ generateID();
+        extraProps['aria-describedby'] = id;
+        if (isString(text)) {
+            textProps._ = text;
+        } else if (isObject(text)) {
+            textProps = {...text};
+        }
+        textProps.id = id;
+    }
     if (ariaDescribedBy) {
         extraProps['aria-describedby'] = ariaDescribedBy;
     }
-    if (type == 'textarea') {
+    if (type === 'textarea') {
+        if (rows) {
+            extraProps['rows'] = rows;
+        }
         this.view(()=> [
-            { input: {
-                    id: props.id,
-                    ...extraProps,
-                    ...baseProps(props, { class: cssClass })
-                } }
+            label ? { [Label]: {
+                ...labelProps
+            }} : null,
+            { textarea: {
+                id: props.id,
+                ...extraProps,
+                ...baseProps(props, { class: cssClass })
+            } },
+            text ? { [Text]: {
+                ...textProps
+            }} : null,
         ]);
         return;
     }
     this.view(()=> [
+        label ? { [Label]: {
+            ...labelProps
+        }} : null,
         { input: {
             id: props.id,
             ...extraProps,
             ...baseProps(props, { class: cssClass })
-        } }
+        } },
+        text ? { [Text]: {
+            ...textProps
+        }} : null,
     ]);
 });
 
