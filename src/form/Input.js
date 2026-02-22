7840a087dd9b6af171fbe0d221dc9c4d7e0cd944
Eduardo Velasques <eduveks@gmail.com>
2026-02-22 20:39:16 +0000

    Input refactored and optimized.

--- src/form/Input.js
+++ src/form/Input.js
@@ -2,20 +2,14 @@ import com from "@xso/com";
 
 import baseProps from "../utils/baseProps";
 import generateID from "../utils/generateID";
-import {isObject, isString} from "@xso/utils";
-import Label from "./Label";
-import Text from "./Text";
 
 const Input = com(function Input(props) {
     const {
         id = 'input_'+ generateID(), placeholder = null,
         type = 'text', size,
-        label = null, text = null,
         ariaLabel = null, ariaDescribedBy = null,
         rows = null
     } = props;
-    let labelProps = {};
-    let textProps = {};
     let cssClass = 'form-control';
     const extraProps = { type };
     extraProps.id = id;
@@ -49,62 +43,31 @@ const Input = com(function Input(props) {
             cssClass += ' form-control-sm';
         }
     }
-    if (label) {
-        if (isString(label)) {
-            labelProps._ = label;
-        } else if (isObject(label)) {
-            labelProps = {...label};
-        }
-        labelProps.for = id;
-    }
     if (ariaLabel) {
         extraProps['aria-label'] = ariaLabel;
-    }
-    if (text) {
-        const id = 'form_text_'+ generateID();
-        extraProps['aria-describedby'] = id;
-        if (isString(text)) {
-            textProps._ = text;
-        } else if (isObject(text)) {
-            textProps = {...text};
-        }
-        textProps.id = id;
     }
     if (ariaDescribedBy) {
         extraProps['aria-describedby'] = ariaDescribedBy;
     }
-    if (type === 'textarea') {
-        if (rows) {
-            extraProps['rows'] = rows;
+    this.view(()=> {
+        if (type === 'textarea') {
+            if (rows) {
+                extraProps['rows'] = rows;
+            }
+            return {
+                textarea: {
+                    id,
+                    ...extraProps,
+                    ...baseProps(props, {class: cssClass})
+                }
+            };
         }
-        this.view(()=> [
-            label ? { [Label]: {
-                ...labelProps
-            }} : null,
-            { textarea: {
-                id: props.id,
-                ...extraProps,
-                ...baseProps(props, { class: cssClass })
-            } },
-            text ? { [Text]: {
-                ...textProps
-            }} : null,
-        ]);
-        return;
-    }
-    this.view(()=> [
-        label ? { [Label]: {
-            ...labelProps
-        }} : null,
-        { input: {
-            id: props.id,
+        return { input: {
+            id,
             ...extraProps,
             ...baseProps(props, { class: cssClass })
-        } },
-        text ? { [Text]: {
-            ...textProps
-        }} : null,
-    ]);
+        } };
+    });
 });
 
 export default Input;
