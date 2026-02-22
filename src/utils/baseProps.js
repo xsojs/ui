8e7de715ba4a3477542a609ab48d9fd19543ed18
Eduardo Velasques <eduveks@gmail.com>
2026-02-22 20:36:34 +0000

    Base Props utils optimized.

--- src/utils/baseProps.js
+++ src/utils/baseProps.js
@@ -5,12 +5,12 @@ function baseProps(props, base = {}) {
         newProps['id'] = props.id;
     }
     if (props.class) {
-        if (base.class && base.class != '') {
+        if (base.class && base.class !== '') {
             newProps['class'] = base.class.trim() +' '+ props.class.trim();
         } else {
             newProps['class'] = props.class.trim();
         }
-    } else if (base.class && base.class != '') {
+    } else if (base.class && base.class !== '') {
         newProps['class'] = base.class.trim();
     }
     if (props.style) {
@@ -18,41 +18,41 @@ function baseProps(props, base = {}) {
     }
     for (const _key of Object.keys(props)) {
         const key = _key.toLowerCase();
-        if (key == 'onclick'
-            || key == 'ondblclick'
-            || key == 'onblur'
-            || key == 'onfocus'
-            || key == 'onfocusin'
-            || key == 'onfocusout'
-            || key == 'ongotpointercapture'
-            || key == 'onlostpointercapture'
-            || key == 'onmousein'
-            || key == 'onmousedown'
-            || key == 'onmouseenter'
-            || key == 'onmouseleave'
-            || key == 'onmousemove'
-            || key == 'onmouseout'
-            || key == 'onmouseover'
-            || key == 'onmouseup'
-            || key == 'oninput'
-            || key == 'onchange'
-            || key == 'onkeydown'
-            || key == 'onkeyup'
-            || key == 'onpointercancel'
-            || key == 'onpointerdown'
-            || key == 'onpointerenter'
-            || key == 'onpointerleave'
-            || key == 'onpointermove'
-            || key == 'onpointerout'
-            || key == 'onpointerover'
-            || key == 'onpointerup'
-            || key == 'onscrool'
-            || key == 'onscroolend'
-            || key == 'ontouchcancel'
-            || key == 'ontouchend'
-            || key == 'ontouchmove'
-            || key == 'ontouchstart'
-            || key == 'onwheel') {
+        if (key === 'onclick'
+            || key === 'ondblclick'
+            || key === 'onblur'
+            || key === 'onfocus'
+            || key === 'onfocusin'
+            || key === 'onfocusout'
+            || key === 'ongotpointercapture'
+            || key === 'onlostpointercapture'
+            || key === 'onmousein'
+            || key === 'onmousedown'
+            || key === 'onmouseenter'
+            || key === 'onmouseleave'
+            || key === 'onmousemove'
+            || key === 'onmouseout'
+            || key === 'onmouseover'
+            || key === 'onmouseup'
+            || key === 'oninput'
+            || key === 'onchange'
+            || key === 'onkeydown'
+            || key === 'onkeyup'
+            || key === 'onpointercancel'
+            || key === 'onpointerdown'
+            || key === 'onpointerenter'
+            || key === 'onpointerleave'
+            || key === 'onpointermove'
+            || key === 'onpointerout'
+            || key === 'onpointerover'
+            || key === 'onpointerup'
+            || key === 'onscrool'
+            || key === 'onscroolend'
+            || key === 'ontouchcancel'
+            || key === 'ontouchend'
+            || key === 'ontouchmove'
+            || key === 'ontouchstart'
+            || key === 'onwheel') {
             newProps[key] = props[_key];
         }
     }
